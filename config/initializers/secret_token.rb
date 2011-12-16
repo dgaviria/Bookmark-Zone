a47068c09dafd4a8746e0815edfcdbf1f8e95659
Daniel Gaviria <ggdaniel@gmail.com>
2011-12-16 16:10:36 -0500

    rails

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-BookmarkZone::Application.config.secret_token = '7ed2c6c5053728a34fdf1b3d29fcfcbd332944b12ee5886cca2b3a517310f05c9587bcbb6651877e32c983c6c91ef6c44d552631edf98d1220be2e58fca7b555'
+BookmarkZone::Application.config.secret_token = 'd9167f51af1092355c71445055cce138e7ca843c87434100702826f99c4d3c006c620a9d08b5e18cd113d8307f54146c7ce1e2cf7869defe2396f3657614407c'

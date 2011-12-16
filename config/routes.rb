2d59277443b6d659e4f3768bb22668e3f16b18b4
Michael <mars8082686@gmail.com>
2011-12-16 16:00:02 -0500

    fixing problems

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
-BookmarkZone::Application.routes.draw do
+Bookmarkzone::Application.routes.draw do
+
+  match '/about',   :to => 'pages#about'
+
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

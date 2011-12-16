411162c1ca4edbef97a67fd8587d1cbe31b6df69
Michael <mars8082686@gmail.com>
2011-12-16 15:03:54 -0500

    deleted troublesome repository

--- bookmarkZone/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-BookmarkZone::Application.config.secret_token = 'ef9fdb868a32285075937f75351010a5750f1b8d4f1a7737c5dbeafa1d4c057151c1df1d0d566c275c8eb6dd06fbdade927d2ebb37c18445dc44cfe4ed98d3a5'

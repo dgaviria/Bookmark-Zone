901b1ce45cdc759caa9faaa8f67926e216f48fd4
Michael <mars8082686@gmail.com>
2011-12-21 18:21:46 -0500

    Added pictures to application

--- app/models/mark.rb
+++ app/models/mark.rb
@@ -1,7 +1,9 @@
 class Mark < ActiveRecord::Base
   belongs_to :book
   has_attached_file :image, :styles => { :large => "600x450>", :medium => "300x300>", :thumb => "100x100>" }
-  attr_accessible :page, :note
+  # in order for this to work you must have imagemagick installed
+  # brew install imagemagick
+  attr_accessible :page, :note, :image
   
   validates :page, :presence => true,
                     :length => { :maximum => 10 }

3d34e3cb752785c4b961c04dbb97e017bacb62bb
Michael <mars8082686@gmail.com>
2011-12-21 16:46:25 -0500

    Cleaned up goodreadswidget code. Placed it in a helper method and removed unnessacary partial

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -4,4 +4,24 @@ module BooksHelper
     User.find(@book.user_id) == currentUser
   end
   
+  def good_reads_widget
+    require 'open-uri'
+
+    begin
+
+    	bookuri = 'http://www.goodreads.com/book/title?format=json&author=' + @book.author + '&title=' + @book.title + '&key=LNsDBhLQTvWd1JSdLIUXQ'
+    	j = open bookuri.tr(' ','+')
+
+    	string = j.string
+
+    	allReviews = ActiveSupport::JSON.decode(string)
+    	allReviewsCode = allReviews["reviews_widget"]
+
+    rescue Exception=>e
+    	allReviewsCode = "Could not Good Reads"
+    end
+    
+    return allReviewsCode
+  end
+    
 end
